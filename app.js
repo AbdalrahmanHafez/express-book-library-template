be6287135d16d5472e816cb9f140435dd4cf6f01
Abdelrahman Hafez <dahomy999@outlook.sa>
2024-12-17 10:54:01 +0200

    customer

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var bookRouter = require('./routes/book.js');
+var customerRouter = require('./routes/customer.js');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/book', bookRouter);
+app.use('/customer', customerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
